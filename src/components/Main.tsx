3e4d9922d5da5ceeac6a379a6f87f4652f120948
mokunshao <mokunshao@foxmail.com>
2020-03-05 01:16:31 +0800

    添加并完善 MainBody 组件

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainHead from './MainHead';
+import MainBody from './MainBody';
 
 export const Main: React.FC = () => {
   return (
     <div>
       <MainHead />
+      <MainBody />
     </div>
   );
 };
---
+++ src/components/MainBody.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Button, Alert, Table } from 'antd';
+
+const columns = [
+  {
+    title: '序号',
+    dataIndex: 'index',
+  },
+  {
+    title: '物资品名',
+    dataIndex: 'name',
+  },
+  {
+    title: '规格/型号',
+    dataIndex: 'model',
+  },
+  {
+    title: '储备类型',
+    dataIndex: 'type',
+  },
+  {
+    title: '是否有效',
+    dataIndex: 'isValid',
+  },
+  {
+    title: '排序',
+    dataIndex: 'sort',
+  },
+  {
+    title: '操作',
+    dataIndex: 'operation',
+  },
+];
+
+const data: Array<any> = [];
+
+for (let i = 0; i < 20; i++) {
+  data.push({
+    key: i,
+    index: i + 1,
+    name: `救援物资 ${i + 1}`,
+    model: 32,
+    type: '不知道',
+    isValid: '是',
+    sort: 10,
+    operation: <Button type="primary">编辑</Button>,
+  });
+}
+
+export const MainBody: React.FC = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState<Array<any>>([]);
+  const [loading, setLoading] = useState(false);
+  const onSelectChange = (selectedRowKeys: Array<any>) => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    setSelectedRowKeys(selectedRowKeys);
+  };
+
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  const hasSelected = selectedRowKeys.length > 0;
+
+  const alertMessage = (
+    <div>
+      已选择 {selectedRowKeys.length} 项{' '}
+      <a onClick={() => setSelectedRowKeys([])}>清空</a>
+    </div>
+  );
+
+  return (
+    <div>
+      <div style={{ marginBottom: 16 }}>
+        <Button type="primary">新增</Button>
+        <Button danger>删除</Button>
+      </div>
+
+      <div style={{ marginBottom: 16 }}>
+        {hasSelected && <Alert message={alertMessage} type="info" showIcon />}
+      </div>
+      <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
+    </div>
+  );
+};
+
+export default MainBody;
