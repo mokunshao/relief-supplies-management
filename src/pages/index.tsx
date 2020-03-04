7734282dd26cc4e5419ebc5c484f1c08f3b97cda
mokunshao <mokunshao@foxmail.com>
2020-03-04 20:39:37 +0800

    删除默认样式和默认内容

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import styles from './index.less';
+import './index.less';
 
 export default () => {
-  return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
-  );
+  return <div>test</div>;
 };
