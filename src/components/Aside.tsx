12605e06e0583545b6bfd99715f322409835f327
mokunshao <mokunshao@foxmail.com>
2020-03-04 21:53:07 +0800

    添加 Aside 和 Main 组件

---
+++ src/components/Aside.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Aside: React.FC = () => {
+  return <div>Aside</div>;
+};
+
+export default Aside;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import './index.less';
+import Aside from '@/components/Aside';
+import Main from '@/components/Main';
 
 export default () => {
-  return <div>test</div>;
+  return (
+    <div className="index">
+      <div className="index-aside">
+        <Aside></Aside>
+      </div>
+      <div className="index-main">
+        <Main></Main>
+      </div>
+    </div>
+  );
 };
