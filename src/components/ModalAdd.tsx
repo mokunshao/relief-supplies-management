d3b77e17c73e8a61a76760213d00cf6154bce68e
mokunshao <mokunshao@foxmail.com>
2020-03-05 14:47:58 +0800

    ModalAdd 添加表单验证

--- src/components/ModalAdd.tsx
+++ src/components/ModalAdd.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Form, Input, Radio, Select } from 'antd';
+import { Modal, Form, Input, Radio, Select, Row, Col } from 'antd';
 
 const { TextArea } = Input;
 
@@ -18,14 +18,16 @@ export const ModalAdd: React.FC<Props> = props => {
   const resetFormData = () => form.resetFields();
 
   const handleOk = () => {
-    setVidsible(false);
-    const formData = form.getFieldsValue();
-    const formData2 = {
-      ...formData,
-      createdTime: Date.now(),
-    };
-    console.log(formData2);
-    resetFormData();
+    form.validateFields().then(() => {
+      const formData = form.getFieldsValue();
+      const formData2 = {
+        ...formData,
+        createdTime: Date.now(),
+      };
+      console.log(formData2);
+      setVidsible(false);
+      resetFormData();
+    });
   };
 
   const handleCancel = () => {
@@ -39,73 +41,79 @@ export const ModalAdd: React.FC<Props> = props => {
       visible={visible}
       onOk={handleOk}
       onCancel={handleCancel}
+      width="50%"
     >
       <Form form={form}>
-        <Form.Item
-          label="物资品名"
-          name="name"
-          rules={[{ required: true, message: '请输入物资品名!' }]}
-        >
-          <Input />
-        </Form.Item>
+        <Row>
+          <Col span={12}>
+            <Form.Item
+              label="物资品名"
+              name="name"
+              rules={[{ required: true, message: '请输入物资品名!' }]}
+            >
+              <Input />
+            </Form.Item>
 
-        <Form.Item label="计量单位" name="unit">
-          <Input />
-        </Form.Item>
+            <Form.Item label="计量单位" name="unit">
+              <Input />
+            </Form.Item>
 
-        <Form.Item
-          label="是否有效"
-          name="isValid"
-          rules={[{ required: true, message: '请输入是否有效!' }]}
-        >
-          <Radio.Group>
-            <Radio value={false}>否</Radio>
-            <Radio value={true}>是</Radio>
-          </Radio.Group>
-        </Form.Item>
+            <Form.Item
+              label="是否有效"
+              name="isValid"
+              rules={[{ required: true, message: '请输入是否有效!' }]}
+            >
+              <Radio.Group>
+                <Radio value={false}>否</Radio>
+                <Radio value={true}>是</Radio>
+              </Radio.Group>
+            </Form.Item>
 
-        <Form.Item
-          label="排序"
-          name="sort"
-          rules={[{ required: true, message: '请输入排序!' }]}
-        >
-          <Input placeholder="请输入排序" />
-        </Form.Item>
+            <Form.Item
+              label="排序"
+              name="sort"
+              rules={[{ required: true, message: '请输入排序!' }]}
+            >
+              <Input placeholder="请输入排序" />
+            </Form.Item>
 
-        <Form.Item label="体积" name="volume">
-          <Input placeholder="请输入体积" />
-        </Form.Item>
+            <Form.Item label="体积" name="volume">
+              <Input placeholder="请输入体积" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="物资类别" name="category">
+              <Input />
+            </Form.Item>
 
-        <Form.Item label="物资类别" name="category">
-          <Input />
-        </Form.Item>
+            <Form.Item
+              label="规格/型号"
+              name="model"
+              rules={[{ required: true, message: '请输入规格/型号!' }]}
+            >
+              <Input placeholder="请输入规格/型号" />
+            </Form.Item>
 
-        <Form.Item
-          label="规格/型号"
-          name="model"
-          rules={[{ required: true, message: '请输入规格/型号!' }]}
-        >
-          <Input placeholder="请输入规格/型号" />
-        </Form.Item>
+            <Form.Item
+              label="储备类型"
+              name="reserve type"
+              rules={[{ required: true, message: '请输入储备类型!' }]}
+            >
+              <Select placeholder="请输入储备类型">
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="Yiminghe">yiminghe</Option>
+              </Select>
+            </Form.Item>
 
-        <Form.Item
-          label="储备类型"
-          name="reserve type"
-          rules={[{ required: true, message: '请输入储备类型!' }]}
-        >
-          <Select placeholder="请输入储备类型">
-            <Option value="jack">Jack</Option>
-            <Option value="lucy">Lucy</Option>
-            <Option value="Yiminghe">yiminghe</Option>
-          </Select>
-        </Form.Item>
-
-        <Form.Item label="重量" name="weight">
-          <Input placeholder="请输入重量" />
-        </Form.Item>
+            <Form.Item label="重量" name="weight">
+              <Input placeholder="请输入重量" />
+            </Form.Item>
+          </Col>
+        </Row>
 
         <Form.Item label="物资用途" name="usage">
-          <TextArea placeholder="请输入物资用途" />
+          <TextArea placeholder="请输入物资用途" style={{ width: '100%' }} />
         </Form.Item>
       </Form>
     </Modal>
