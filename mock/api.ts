bfca9aab96bf61b53134fd31013a9c3d3191bf6b
mokunshao <mokunshao@foxmail.com>
2020-03-06 17:51:37 +0800

    修改  /api/items/getByConditions 接口逻辑

--- mock/api.ts
+++ mock/api.ts
@@ -73,6 +73,7 @@ export default {
   },
   'POST /api/items/getByConditions': (req: any, res: any) => {
     let { model, isValid, typeKey } = req.body;
+    console.log(req.body);
     let result;
     const model2 = parseInt(model); // 0 1 2 3
     const isValid2 = Boolean(parseInt(isValid)); // true false
@@ -93,9 +94,11 @@ export default {
       });
     }
 
-    result = result.filter((element: any) => {
-      return element.type.key === typeKey;
-    });
+    if (typeKey !== '0') {
+      result = result.filter((element: any) => {
+        return element.type.key === typeKey;
+      });
+    }
 
     res.send(result);
   },
